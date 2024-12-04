cbd63f100e8d37b8b041342383a754a2854b2353
Davide Galassi <davxy@datawok.net>
2024-12-04 16:43:14 +0100

    Max tickets per block const

--- jam_types/const.py
+++ jam_types/const.py
@@ -1,10 +1,12 @@
 # Full Spec
 # validators_count = 1023
 # epoch_length = 600
+# max_tickets_per_block = 16
 
 # Tiny Spec
 validators_count = 6
 epoch_length = 12
+max_tickets_per_block = 3
 
 validators_per_core = 3
 
@@ -14,5 +16,3 @@ validators_super_majority = validators_count // 3 * 2 + 1
 avail_bitfield_bytes = (core_count + 7) // 8
 
 hash_size = 32
-
-max_tickets_per_block = 16
