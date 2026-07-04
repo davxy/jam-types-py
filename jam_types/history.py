0ec049a083011c2bfaa01d88d65bf74c3610f17c
Davide Galassi <davxy@datawok.net>
2026-07-04 09:23:35 +0200

    Timeslot in block history item

--- jam_types/history.py
+++ jam_types/history.py
@@ -1,4 +1,4 @@
-from .simple import Struct, OpaqueHash, BoundedVec, n
+from .simple import Struct, OpaqueHash, BoundedVec, TimeSlot, n
 from .spec import recent_blocks_max_size
 
 class Mmr(Struct):
@@ -18,6 +18,7 @@ class BlockInfo(Struct):
         ('header_hash', n(OpaqueHash)),
         ('beefy_root', n(OpaqueHash)),
         ('state_root', n(OpaqueHash)),
+        ('slot', n(TimeSlot)),
         ('reported', 'Vec<ReportedWorkPackage>')
     ]
 
